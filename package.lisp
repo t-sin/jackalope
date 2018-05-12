678b6905187a6785561238c99bccc3177980b849
t-sin <shinichi.tanaka45@gmail.com>
2018-05-12 23:54:12 +0900

    Exports disassembler functions

--- package.lisp
+++ package.lisp
@@ -3,5 +3,10 @@
   (:use #:cl)
   (:import-from #:jackalope-class-parser
                 #:read-class-file)
-  (:export #:read-class-file))
+  (:import-from #:jackalope-disassembler
+                #:disassemble-bytecode
+                #:format-bytecode)
+  (:export #:read-class-file
+           #:disassemble-bytecode
+           #:format-bytecode))
 (in-package #:jackalope)
