6b40072568d61607ffa0779f1b2cf19ee8d18a76
t-sin <shinichi.tanaka45@gmail.com>
2018-05-12 22:09:01 +0900

    What's this?

--- package.lisp
+++
@@ -1,7 +0,0 @@
-(in-package #:cl-user)
-(defpackage #:jackalope
-  (:use #:cl)
-  (:import-from #:jackalope-class-parser
-                #:read-class-file)
-  (:export #:read-class-file))
-(in-package #:jackalope)
