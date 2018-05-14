9677596b5e86901708ff4292ccd9cd430317c403
t-sin <shinichi.tanaka45@gmail.com>
2018-05-14 23:26:23 +0900

    Exports class file accessors

--- class-parser/package.lisp
+++ class-parser/package.lisp
@@ -1,8 +1,30 @@
 (in-package #:cl-user)
 (defpackage #:jackalope-class-parser
   (:use #:cl)
+  (:import-from #:jackalope-class-parser/class-parser/classfile
+                #:make-classfile
+                #:classfile-version
+                #:classfile-constant-pool
+                #:classfile-access-flags
+                #:classfile-this
+                #:classfile-super
+                #:classfile-interfaces
+                #:classfile-fields
+                #:classfile-methods
+                #:classfile-attributes)
   (:import-from #:jackalope-class-parser/class-parser/parser
                 #:read-class-file)
-  (:export #:read-class-file))
+  (:export #:make-classfile
+           #:classfile-version
+           #:classfile-constant-pool
+           #:classfile-access-flags
+           #:classfile-this
+           #:classfile-super
+           #:classfile-interfaces
+           #:classfile-fields
+           #:classfile-methods
+           #:classfile-attributes
+
+           #:read-class-file))
 (in-package #:jackalope-class-parser)
 
--- package.lisp
+++ package.lisp
@@ -2,11 +2,33 @@
 (defpackage #:jackalope
   (:use #:cl)
   (:import-from #:jackalope-class-parser
+                #:make-classfile
+                #:classfile-version
+                #:classfile-constant-pool
+                #:classfile-access-flags
+                #:classfile-this
+                #:classfile-super
+                #:classfile-interfaces
+                #:classfile-fields
+                #:classfile-methods
+                #:classfile-attributes
+
                 #:read-class-file)
   (:import-from #:jackalope-disassembler
                 #:disassemble-bytecode
                 #:format-bytecode)
-  (:export #:read-class-file
+  (:export #:make-classfile
+           #:classfile-version
+           #:classfile-constant-pool
+           #:classfile-access-flags
+           #:classfile-this
+           #:classfile-super
+           #:classfile-interfaces
+           #:classfile-fields
+           #:classfile-methods
+           #:classfile-attributes
+           #:read-class-file
+
            #:disassemble-bytecode
            #:format-bytecode))
 (in-package #:jackalope)
