6e3f83da2cca08c470fd4a27a3217db99760b0f6
t-sin <shinichi.tanaka45@gmail.com>
2018-05-22 23:47:24 +0900

    Exports method structure

--- classfile/package.lisp
+++ classfile/package.lisp
@@ -1,24 +1,38 @@
 (in-package #:cl-user)
 (defpackage #:jackalope-classfile
   (:use #:cl)
-  (:import-from #:jackalope-classfile/classfile/classfile
-                #:make-classfile
-                #:classfile-version
-                #:classfile-constant-pool
-                #:classfile-access-flags
-                #:classfile-this
-                #:classfile-super
-                #:classfile-interfaces
-                #:classfile-fields
-                #:classfile-methods
-                #:classfile-attributes)
+  (:shadowing-import-from #:jackalope-classfile/classfile/classfile
+                          #:method
+                          #:method-access-flags
+                          #:method-name
+                          #:method-descriptor
+                          #:method-attributes
+
+                          #:classfile
+                          #:make-classfile
+                          #:classfile-version
+                          #:classfile-constant-pool
+                          #:classfile-access-flags
+                          #:classfile-this
+                          #:classfile-super
+                          #:classfile-interfaces
+                          #:classfile-fields
+                          #:classfile-methods
+                          #:classfile-attributes)
   (:import-from #:jackalope-classfile/classfile/parser
                 #:read-class-file)
   (:shadowing-import-from #:jackalope-classfile/classfile/accessor
                           #:find-attribute
                           #:find-field
                           #:find-method)
-  (:export #:make-classfile
+  (:export #:method
+           #:method-access-flags
+           #:method-name
+           #:method-descriptor
+           #:method-attributes
+
+           #:classfile
+           #:make-classfile
            #:classfile-version
            #:classfile-constant-pool
            #:classfile-access-flags
